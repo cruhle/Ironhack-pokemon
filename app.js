58ad04e4f2fcb7376cd2fdffd97c1cc40d007f5c
cruhle <carl_ruhle@yahoo.com>
2020-08-27 23:05:17 +0100

    files upload by browser

---
+++ app.js
@@ -0,0 +1,29 @@
+let contador=1
+
+function render) {
+	let pokemon=document.getElementById("pokemon")
+	pokemon.innerHTML=`<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${contador}.svg">`	
+}
+
+let prev=document.getElementById("prev")
+
+let next=document.getElementById("next")
+
+prev.onclick = function() {
+	contador-=1
+	if(contador==0) {
+		contador=650
+	}
+	render()
+}
+
+next.onclick = function() {
+	contador+=1
+	if(contador>650) {
+		contador=1
+	}
+	render()
+}
+
+
+render()
